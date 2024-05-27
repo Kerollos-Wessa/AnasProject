fdf835ebe614342a2c7c864bdf346f8f0768f477
Mohammed Ali <93165125+mohammedali882001@users.noreply.github.com>
2024-05-26 20:16:04 -0800

    validations

--- DTOS/DriverDto.cs
+++ DTOS/DriverDto.cs
@@ -5,9 +5,12 @@ namespace AnasProject.DTOS
     public class DriverDTO
     {
         [Required]
+        [MaxLength(25)]
+        [MinLength(2)]
         public string DriverName { get; set; }
 
         [Required]
+        [PhoneNumberValidation]
         public string PhoneNumber { get; set; }
     }
 }
